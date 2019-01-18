47af47153b5b0ea22163361d4c5750b430f89b25
Kélyan Dhedin <kelyan.dhedin@103f12.iut-lens.univ-artois.fr>
2019-01-18 16:00:27 +0100

    game over, contraintes et pillules

--- app/src/main/java/com/example/kelyandhedin/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/kelyandhedin/myfirstapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kelyandhedin.myfirstapplication;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,10 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private int distance_menace = -50;
     private String text_distance;
     private String text_ecart;
-    private int fatigue = 0;
+    private int etat_fatigue = 0;
     private int soif = 0;
     private int eau = 100;
-    private int ecart = 0;
+    private int pillule = 3;
+    private int ecart = 50;
 
 
     @Override
@@ -42,6 +44,31 @@ public class MainActivity extends AppCompatActivity {
 
        */
 
+    public void DangerColor(View view , int ecart) {
+
+        ProgressBar progressBarEcart = findViewById(R.id.progressBarEcart);
+
+        Drawable progressDrawable = progressBarEcart.getProgressDrawable().mutate();
+
+        if (ecart <= 15){
+
+            progressDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+            progressBarEcart.setProgressDrawable(progressDrawable);
+
+        }else if (ecart <= 30 && ecart >= 16){
+
+            progressDrawable.setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
+            progressBarEcart.setProgressDrawable(progressDrawable);
+
+        }else if (ecart >= 31){
+
+            progressDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
+            progressBarEcart.setProgressDrawable(progressDrawable);
+
+        }
+
+    }
+
     public void Action(View view) {
 
         ProgressBar progressBarEcart = findViewById(R.id.progressBarEcart);
@@ -63,82 +90,56 @@ public class MainActivity extends AppCompatActivity {
 
         if (view.getId() == R.id.bouton_marcher){
 
-            distance += i1;
-            fatigue += 10;
-            soif += 10;
-            distance_menace += i2;
-            ecart= distance-distance_menace;
+            if (etat_fatigue <= 100) {
 
-            ProgressBar progressBarDistance = findViewById(R.id.progressBarDistance);
-            progressBarDistance.setProgress(distance);
+                distance += i1;
+                etat_fatigue += 10;
+                soif += 10;
+                distance_menace += i2;
+                ecart = distance - distance_menace;
 
-            ProgressBar progressBarFatigue = findViewById(R.id.progressBarFatigue);
-            progressBarFatigue.setProgress(fatigue);
+                ProgressBar progressBarDistance = findViewById(R.id.progressBarDistance);
+                progressBarDistance.setProgress(distance);
 
-            ProgressBar progressBarSoif = findViewById(R.id.progressBarSoif);
-            progressBarSoif.setProgress(soif);
+                ProgressBar progressBarFatigue = findViewById(R.id.progressBarFatigue);
+                progressBarFatigue.setProgress(etat_fatigue);
+
+                ProgressBar progressBarSoif = findViewById(R.id.progressBarSoif);
+                progressBarSoif.setProgress(soif);
 
 
-            progressBarEcart.setProgress(100-(2*ecart));
+                progressBarEcart.setProgress(100 - (2 * ecart));
 
-            if (ecart <= 15){
+                DangerColor(view, ecart);
 
-                progressDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
+                TextView textViewDistance = findViewById(R.id.TextDistance);
+                text_distance = distance + "/120 km";
+                textViewDistance.setText(text_distance);
 
-            }else if (ecart <= 30 && ecart >= 16){
-
-                progressDrawable.setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
-
-            }else if (ecart >= 31){
-
-                progressDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
-
+                TextView textView2 = findViewById(R.id.ecart_menace);
+                text_ecart = (distance - distance_menace) + "km";
+                textView2.setText(text_ecart);
+            }else{
+                //bruit, animation ou message
             }
-
-            TextView textViewDistance = findViewById(R.id.TextDistance);
-            text_distance = distance+"/120 km";
-            textViewDistance.setText(text_distance);
-
-            TextView textView2 = findViewById(R.id.ecart_menace);
-            text_ecart = (distance-distance_menace)+"km";
-            textView2.setText(text_ecart);
 
         }
 
         if (view.getId() == R.id.bouton_repos){
 
             distance_menace += 8;
-            fatigue += (-10);
-            soif += 20;
+            etat_fatigue += (-10);
             ecart= distance-distance_menace;
 
             ProgressBar progressBarFatigue = findViewById(R.id.progressBarFatigue);
-            progressBarFatigue.setProgress(fatigue);
+            progressBarFatigue.setProgress(etat_fatigue);
 
             ProgressBar progressBarSoif = findViewById(R.id.progressBarSoif);
             progressBarSoif.setProgress(soif);
 
             progressBarEcart.setProgress(100-(2*ecart));
 
-            if (ecart <= 15){
-
-                progressDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
-
-            }else if (ecart <= 30 && ecart >= 16){
-
-                progressDrawable.setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
-
-            }else if (ecart >= 31){
-
-                progressDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
-
-            }
+            DangerColor(view,ecart);
 
             TextView textView = findViewById(R.id.ecart_menace);
             text_ecart = (distance-distance_menace)+"km";
@@ -148,40 +149,60 @@ public class MainActivity extends AppCompatActivity {
 
         if (view.getId() == R.id.bouton_boire){
 
-            distance_menace += 3;
-            eau += (-15);
-            soif += (-10);
-            ecart= distance-distance_menace;
+            if (eau > 0) {
 
-            ProgressBar progessBarEau = findViewById(R.id.progressBarEau);
-            progessBarEau.setProgress(eau);
+                distance_menace += 3;
+                eau += (-15);
+                soif += (-10);
+                ecart = distance - distance_menace;
 
-            ProgressBar progressBarSoif = findViewById(R.id.progressBarSoif);
-            progressBarSoif.setProgress(soif);
+                ProgressBar progessBarEau = findViewById(R.id.progressBarEau);
+                progessBarEau.setProgress(eau);
 
-            progressBarEcart.setProgress(100-(2*ecart));
+                ProgressBar progressBarSoif = findViewById(R.id.progressBarSoif);
+                progressBarSoif.setProgress(soif);
 
-            if (ecart <= 15){
+                progressBarEcart.setProgress(100 - (2 * ecart));
 
-                progressDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
+                DangerColor(view, ecart);
 
-            }else if (ecart <= 30 && ecart >= 16){
-
-            progressDrawable.setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
-            progressBarEcart.setProgressDrawable(progressDrawable);
-
-            }else if (ecart >= 31){
-
-                progressDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
-                progressBarEcart.setProgressDrawable(progressDrawable);
-
+                TextView textView = findViewById(R.id.ecart_menace);
+                text_ecart = (distance - distance_menace) + "km";
+                textView.setText(text_ecart);
+            }else{
+                //bruit, animation ou message
             }
 
-            TextView textView = findViewById(R.id.ecart_menace);
-            text_ecart = (distance-distance_menace)+"km";
-            textView.setText(text_ecart);
+        }
 
+        if (view.getId() == R.id.bouton_drogue){
+
+            if (pillule > 0) {
+
+                distance_menace += 3;
+                ecart = distance - distance_menace;
+
+                ProgressBar progressBarPillule = findViewById(R.id.progressBarPillule);
+                progressBarPillule.setProgress(pillule);
+
+                progressBarEcart.setProgress(100 - (2 * ecart));
+
+                DangerColor(view, ecart);
+
+                TextView textView = findViewById(R.id.ecart_menace);
+                text_ecart = (distance - distance_menace) + "km";
+                textView.setText(text_ecart);
+
+            }else{
+                //bruit, animation ou message
+            }
+
+        }
+
+        if(ecart <= 0){
+            Intent intent= new Intent(this,Main2Activity.class);
+            startActivity(intent);
+            //GAME OVER
         }
 
 
