b70f07bff50c1256a3bf7c46244a70f16607bbbf
maxime.manicourt <m.manicourt@gmail.com>
2019-03-14 11:59:15 +0100

    splash screen and game over screen

--- app/src/main/java/com/example/kelyandhedin/myfirstapplication/Main2Activity.java
+++ app/src/main/java/com/example/kelyandhedin/myfirstapplication/Main2Activity.java
@@ -9,18 +9,7 @@ import android.widget.TextView;
 public class Main2Activity extends AppCompatActivity {
 
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
-public class Main2Activity extends AppCompatActivity {
-
-
     //String strName= getIntent().getExtras().getString("END");
-
-
 
 
     @Override
@@ -28,31 +17,28 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-    }
-}
 
-
-        String newString= " ";
+        String newString = " ";
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if (extras == null) {
                 newString = null;
             } else {
-                newString = extras.getString("END","you loose");
+                newString = extras.getString("END", "you loose");
             }
         }
 
 
-
         TextView textViewEND = findViewById(R.id.textViewEND);   //affichage de l''écart
-        String text_fin = newString ;
+        String text_fin = newString;
         textViewEND.setText(text_fin);
     }
 
 
     public void RetourMenu(View view) {
-        Intent intent = new Intent(this,StartActivity.class);
+        Intent intent = new Intent(this, StartActivity.class);
         startActivity(intent);
 
     }
+}
 
